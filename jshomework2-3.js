c180fbe451722e519356ed27cb7cb565d4a0a36a
Carson Hamilton <cjcham77@gmail.com>
2016-11-03 16:27:31 -0600

    redo of the assignment

--- jshomework2-3.js
+++ jshomework2-3.js
@@ -1,3 +1,5 @@
 document.getElementsByTagName('Body')[0].addEventListener('keypress', function(){
 	document.getElementById('keys').innerText = String.fromCharCode(event.keyCode);
-});
+})
+//This takes the body and adds an event listener to it that takes in key presses
+// and puts them through Charcode to type out the character on the screen.
